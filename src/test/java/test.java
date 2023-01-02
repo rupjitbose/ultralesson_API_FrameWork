837b8f71e35b3b5438d8219072027a2413b2aac2
rupjitbose <boserupjit13@gmail.com>
2023-01-02 12:13:09 +0530

    new commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,6 +1,15 @@
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.given;
+
 public class test {
 
+    @Test
+    public void shouldReturnAllUsers(){
+        given()
+                .when().get("https://gorest.co.in/public/v1/users/")
+                .then().statusCode(200)
+                .log().body();
+    }
 
 }
